3dd570c46010df66ca2971d8654d94f80b0e1d12
KylianCrv <kylian.crv@hotmail.com>
2022-07-21 15:20:02 +0200

    exercice 1

--- unitest/src/test/java/fr/m2i/unitest/JavaUnitTest.java
+++ unitest/src/test/java/fr/m2i/unitest/JavaUnitTest.java
@@ -4,7 +4,11 @@
  */
 package fr.m2i.unitest;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,8 +17,40 @@ import org.junit.jupiter.api.Test;
  */
 public class JavaUnitTest {
 
+//    @Test
+//    public void firstTestMethod() {
+//        assertTrue(true);
+//    }
+    @BeforeAll
+    static void setupAll() {
+        System.out.println("setupAll");
+    }
+
+    @BeforeEach
+    void setup() {
+        System.out.println("setup");
+    }
+
     @Test
-    public void firstTestMethod() {
+    void unTest() {
+        System.out.println("un Test");
         assertTrue(true);
     }
+
+    @Test
+    void unAutreTest() {
+        System.out.println("un autre test");
+        assertTrue(true);
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("teardown");
+    }
+
+    @AfterAll
+    static void teardownAll() {
+        System.out.println("teardownAll");
+    }
+
 }
