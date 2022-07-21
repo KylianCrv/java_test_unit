774dc5d5e07316afd4e218e389b2112997f4f364
KylianCrv <kylian.crv@hotmail.com>
2022-07-21 15:01:37 +0200

    2nd commit

---
+++ unitest/src/test/java/fr/m2i/unitest/JavaUnitTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.m2i.unitest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author ishish
+ */
+public class JavaUnitTest {
+
+    @Test
+    public void firstTestMethod() {
+        assertTrue(true);
+    }
+}
